0a9126cd168d0d27be7ce412a1f6918a73f0b193
Kyle <162121443+KyleRen05@users.noreply.github.com>
2026-02-26 18:04:20 +0200

    redo q1

--- ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q1/WMS_Server.py
+++ ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q1/WMS_Server.py
@@ -1,24 +1,52 @@
 """ SERVER SIDE """
 import socket
-import time
-import threading
 
 SERVER_IP = '127.0.0.1'
 PORT = 5500
 
+server_running = True
+
+items = {
+    "Nails": 200,
+    "Screws": 321,
+    "Toolkit": 59,
+    "Wood Planks": 97,
+    "Sheet Metal": 153,
+    "Lightbulb": 76,
+}
+
 def handle_client(client_socket, client_address):
     print(f"Connected to {client_address}")
+    global server_running
 
     while True:
         try:
-             data = client_socket.recv(1024).decode('utf-8')
-             print(f"Client {client_address} connected successfully!")
+            data = client_socket.recv(1024).decode('utf-8').strip()
+            print(f"Client {client_address} connected successfully!")
+
+            # in case client presses enter
+            if not data:
+                continue
 
              # Client Exit Code
-             if data == "exit":
-                 break
-        except:
-            print("Error")
+            if data == "exit":
+                print(f" [DISCONNECT] {client_address} sent the exit code.")
+                server_running = False
+                break
+
+            # search through dictionary to find item
+            if data in items and items[data] > 0:
+                message = f"[ITEM FOUND] {data} : {items[data]}"
+            else:
+                message = f"[ITEM NOT FOUND] {data} is not in stock"
+
+            client_socket.sendall((message + "\n").encode('utf-8'))
+                    
+        except ConnectionResetError:
+            print(f"Error: Client Closed Connection Upbruptly")
+        except Exception as e:
+            print(f"Error: {e}")
+    client_socket.close()
 
 
 def start_server():
@@ -30,20 +58,42 @@ def start_server():
     server_socket.bind((SERVER_IP, PORT))
 
     server_socket.listen()
+    global server_running
 
-    print(f'=' * 20)
-    print(" TCP Chat Server Started")
-    print("=" * 20)
+    # Allows for keyboard interrupt on server side every 1 second
+    server_socket.settimeout(1.0)
+
+    print(f'=' * 40)
+    print(" TCP WMS Server Started")
+    print("=" * 40)
     print(f" Listening on {SERVER_IP}:{PORT}")
 
     try:
-        while True:
-            client_socket, client_address = server_socket.accept()
+        while server_running == True:
 
+            try:
 
+                client_socket, client_address = server_socket.accept()
+                
+                # When client connects, remove timeout
+                client_socket.settimeout(None)
+
+                handle_client(client_socket, client_address)
+
+                if server_running == False: break
+
+            except socket.timeout:
+                continue
+            
     except KeyboardInterrupt:
         print(f"\n\n Server interrupted by keyboard input")
 
     finally:
-        client_socket.closee()
+        if 'client_socket' in locals():
+            client_socket.close()
         server_socket.close()
+
+        print(f" Server Closed\n")
+
+if __name__ == "__main__":
+    start_server()
--- ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q1/client.py
+++ ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q1/client.py
@@ -1,5 +1,46 @@
 """ CLIENT SIDE """
 import socket
-import time
-import threading
 
+SERVER_IP = '127.0.0.1'
+PORT = 5500
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("=" * 40)
+    print("WMS Client")
+    print("=" * 40)
+
+    try:
+        print(f"Connecting to {SERVER_IP}:{PORT}...")
+        client_socket.connect((SERVER_IP, PORT))
+        print("Connected to server\n")
+
+        while True:
+            print(f"What item are you looking for? ('exit' to quit)")
+            message = input()
+
+            if message.strip().lower() == "exit":
+                print("Disconencting from server")
+                client_socket.sendall("Exit".encode('utf-8'))
+                break
+
+            if message.strip():
+                client_socket.sendall(message.encode('utf-8'))
+
+                reply = client_socket.recv(1024).decode('utf-8')
+                print(reply, end='')
+
+    except ConnectionRefusedError:
+        print(f"[ERROR] Could not connect to server at {SERVER_IP}:{PORT}")
+    
+    except Exception as e:
+        print(f" Error: {e}")
+    
+    finally:
+        # Close connection
+        client_socket.close()
+        print("[DISCONNECT] Successful\n")
+
+if __name__ == "__main__":
+    start_client()
