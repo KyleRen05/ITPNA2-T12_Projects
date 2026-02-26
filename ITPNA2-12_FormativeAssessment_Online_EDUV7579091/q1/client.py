13b58c7971dda505246783b9fa9f3d94e6dea30c
Kyle <162121443+KyleRen05@users.noreply.github.com>
2026-02-26 14:03:54 +0200

    q1 redo

--- ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q1/client.py
+++ ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q1/client.py
@@ -1,36 +1,7 @@
 """ CLIENT SIDE """
 import socket
 import time
+import threading
 
-""" NETWORKING """
 
-CLIENT_IP = '127.0.0.1'
-PORT = 5001
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-""" CLIENT REQUEST """
-#print(f"What item are you looking for? ('exit' to end program)")
-#req_item = input()
-
-try:
-    client.connect((CLIENT_IP, PORT))
-    while True:     
-        print(f"What item are you looking for? ('exit' to end program)")
-        req_item = input()
-        client.send(req_item.encode())
-
-        data = client.recv(1024)
-        print(f"{data.decode()}")
-
-        data = client.recv(1024)
-        print(f"{data.decode()}")
-        
-        if req_item.lower() == "exit":
-            time.sleep(1)
-            break
-        
-except:
-    print(f"Error: Connection to server failed")
-
-client.close()
