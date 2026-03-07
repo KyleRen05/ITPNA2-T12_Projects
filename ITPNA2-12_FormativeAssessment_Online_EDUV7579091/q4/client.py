8742414d5ccd6bd789093ab2c7554c9e7237744e
Kyle <162121443+KyleRen05@users.noreply.github.com>
2026-03-07 14:55:05 +0200

    q4 ftp server continued
    Client server connection established, no file transfer function

--- ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q4/client.py
+++ ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q4/client.py
@@ -6,10 +6,12 @@ SERVER_IP = '127.0.0.1'
 SERVER_PORT = 5001
 
 
-def send_files():
+def send_files(file_path):
+    print(f"[SENDING...] {file_path} to {SERVER_IP}")
     return
 
-def req_files():
+def req_files(file_name):
+    print(f"[REQUEST] {file_name} from {SERVER_IP}")
     return
 
 def start_client():
@@ -26,6 +28,36 @@ def start_client():
         print("Connected to Server\n")
 
         while True:
+            print("Would you like to send or request a file?")
+            print(" (1) SEND FILE\n (2) REQUEST FILE\n (3) EXIT PROGRAM\n")
+            selection = input()
+
+            if selection == '1':
+                client_socket.sendall(selection.encode('utf-8'))
+                print("Please enter file path of the file you want to send:\n")
+                file_path = input()
+                send_files(file_path)
+            elif selection == '2':
+                client_socket.sendall(selection.encode('utf-8'))
+                print("Please enter reuqested file name:\n")
+                file_name = input()
+                req_files(file_name)
+            elif selection == '3':
+                client_socket.sendall(selection.encode('utf-8'))
+                print("[EXIT] Exit Condition Met\nClosing Client Connection")
+                break
+
+
+    except ConnectionRefusedError:
+        print(f"[ERROR] Could not connect to server at {SERVER_IP}:{SERVER_PORT}")
+    
+    except Exception as e:
+        print(f" Error: {e}")
+    
+    finally:
+        # Close connection
+        client_socket.close()
+        print("[DISCONNECT] Successful\n")
             
 
 if __name__ == "__main__":
--- ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q4/server.py
+++ ITPNA2-12_FormativeAssessment_Online_EDUV7579091/q4/server.py
@@ -8,9 +8,12 @@ SERVER_PORT = 5001
 connected_clients = []
 client_names = {}
 
+server_running = True
+
 
 
 def handle_client(client_socket, client_address):
+    global server_running
     # handles connecting clients
     print(f"[NEW CONNECTION] {client_address} connected")
 
@@ -19,8 +22,20 @@ def handle_client(client_socket, client_address):
     try:
         client_name = client_socket.recv(1024).decode('utf-8').strip()
         client_names[client_socket] = client_name
+
+        while True:
+            selection = client_socket.recv(1024).decode('utf-8').strip()
+            if selection == '1':
+                print(f"{selection}")
+            elif selection == '2':
+                print(selection)
+            elif selection == '3':
+                print(selection)
+                server_running = False
+                break
+
     except Exception as e:
-        print(f"[ERROR] e")
+        print(f"[ERROR] {e}")
 
 def send_files():
     return
@@ -29,20 +44,21 @@ def rec_files():
     return
 
 def start_server():
+    global server_running
     # Handles server
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((SERVER_IP, SERVER_PORT))
     server_socket.listen(5)
 
-    print("=" * 60)
-    print("Multi-Client File Transfer Server Started")
-    print("=" * 60)
-    print(f"Listeting on {SERVER_IP}:{SERVER_PORT}")
+    print("=" * 30)
+    print("Multi6Client File Transfer Server Started")
+    print("=" * 30)
+    print(f"List6ting on {SERVER_IP}:{SERVER_PORT}")
     print(f"Waiting for Client Connections...\n")
 
     try:
-        while True:
+        while server_running == True:
             client_socket, client_address = server_socket.accept()
             connected_clients.append(client_socket)
 
